373823bbdac9064b09e6491018a683422ee696ae
Thomas Radloff <bostontrader@gmail.com>
2019-06-25 13:45:44 +0800

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,205 @@
+use actix_web::{web, App, Responder, HttpServer};
+
+use bookwerx_core_rust::constants as C;
+use clap::clap_app;
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn index(info: web::Path<(String, u32)>) -> impl Responder {
+    format!("Hello {}! id:{}", info.0, info.1)
+}
+
+
+fn main() {
+
+    // 1. Configure the CLI
+    let cli_matches = clap_app!(bookwerx_core_rust =>
+        (version: "0.1.2") // Keep this in sync with TOML
+        (author: "Thomas Radloff. <bostontrader@gmail.com>")
+        (about: "A blind man in a dark room looking for a black cat that's not there.")
+        (@arg bind: -b --bind +takes_value "Which IP and port shall the http server bind to? For example 127.0.0.1:3003")
+        (@arg conn: -c --conn +takes_value "Specifies a connection string to connect to the db. Ex: mysql://root:mysecretpassword@127.0.0.1:3306")
+        (@arg db: -d --db +takes_value "The database name to use.")
+        (@arg init: -i --init +takes_value "Initialize the db using the given seed file.")
+    ).get_matches();
+
+
+    // 2. Obtain a connection string, if available.
+    let mut conn_string;
+    match cli_matches.value_of(C::CONN_KEY_CLI) {
+        Some(_x) => {
+            println!("Accessing the db via connection string [{}], as set from the command line.", _x);
+            conn_string = _x.to_string();
+        }
+        None =>
+            match env::var(C::CONN_KEY_ENV) {
+                Ok(_x) => {
+                    println!("Accessing the db via connection string [{}], as set from the environment.", _x);
+                    conn_string = _x;
+                }
+
+                Err(_) => {
+                    println!("Fatal error: No db connection string available.");
+                    ::std::process::exit(1);
+                }
+            }
+    }
+
+    // 3. Obtain a db name, if available.
+    let mut db_name;
+    match cli_matches.value_of(C::DB_KEY_CLI) {
+        Some(_x) => {
+            println!("Using database [{}], as set from the command line.", _x);
+            db_name = _x.to_string();
+        }
+        None =>
+            match env::var(C::DB_KEY_ENV) {
+                Ok(_x) => {
+                    println!("Using database [{}], as set from the environment.", _x);
+                    db_name = _x;
+                }
+
+                Err(_) => {
+                    println!("Fatal error: No database specified.");
+                    ::std::process::exit(1);
+                }
+            }
+    }
+
+    // 4. Obtain a seed_file name for the db, if available.
+    let mut seed_file = String::new();
+    match cli_matches.value_of(C::INIT_KEY_CLI) {
+        Some(_x) => {
+            println!("Initializing the db with seed file [{}], as set from the command line.", _x);
+            seed_file = _x.to_string();
+        }
+        None =>
+            match env::var(C::INIT_KEY_ENV) {
+                Ok(_x) => {
+                    println!("Initializing the db with seed file [{}], as set from the environment.", _x);
+                    seed_file = _x;
+                }
+
+                Err(_) => {
+                  // Neither the command line nor the env says anything about initialization.  Therefore don't do any initialization.
+                }
+        }
+    }
+
+    // 5. If a seed_file is available then try to read it.
+    // The seed file should not be very large and reading it should not be any trouble.
+    let mut seed = String::new();
+    if !seed_file.is_empty() {
+        // Create a path to the desired file
+        let path = Path::new(&seed_file);
+        let display = path.display();
+
+        // Open the path in read-only mode.
+        let mut file = match File::open(&path) {
+            Ok(file) => file,
+
+            Err(why) => {
+                println!("Couldn't open [{}]: {}", display, why.description());
+                ::std::process::exit(1);
+            },
+        };
+
+        // Read the file contents into a string, returns `io::Result<usize>`
+        match file.read_to_string(&mut seed) {
+            Ok(_) => {
+                println!("{} contains:{}", display, seed)
+            }
+            Err(why) => {
+                println!("couldn't read {}: {}", display, why.description());
+                ::std::process::exit(1);
+            }
+        }
+    }
+
+    // 6. Now try to connect to the mysql server
+    match mysql::Conn::new(&conn_string) {
+        Ok(mut _conn) => {
+            println!("Connected to [{}]", conn_string);
+
+            // If there is a seed, wipe the db and re-init.
+            if !seed.is_empty() {
+                match _conn.query(format!("DROP DATABASE IF EXISTS `{0}`; CREATE DATABASE `{0}`;", db_name)) {
+                    Ok(_) => {
+                        println!("drop and create success");
+                    }
+                    Err(_) => {
+                        println!("drop and create fail");
+                        ::std::process::exit(1);
+                    }
+                };
+
+                _conn.select_db(&db_name[..]);
+
+                match _conn.query(&seed) {
+                    Ok(_) => {
+                        println!("The seed has germinated.")
+                    }
+                    Err(_x) => {
+                        println!("The seed file does not contain valid SQL.  Does not compute.");
+                        ::std::process::exit(1);
+                    }
+                };
+            }
+
+            // At this point the db should be there, newly created if need be.
+            if _conn.select_db(&db_name[..]) {
+                println!("USE database [{}] success.", &db_name);
+            } else {
+                println!("Fatal error: Unknown database [{}].", &db_name);
+                ::std::process::exit(1);
+            }
+
+
+        }
+        Err(_err) => {
+            println!("{}", _err);
+            ::std::process::exit(1);
+        }
+
+
+    }
+
+    // 7. Obtain the http server binding, if available.
+    let mut bind_string;
+    match cli_matches.value_of(C::BIND_KEY_CLI) {
+        Some(_x) => {
+            println!("The HTTP server will bind to [{}], as specified from the command line.", _x);
+            bind_string = _x.to_string();
+        }
+        None =>
+            match env::var(C::BIND_KEY_ENV) {
+                Ok(_x) => {
+                    println!("The HTTP server will bind to [{}], as specified in the environment.", _x);
+                    bind_string = _x;
+                }
+
+                Err(_) => {
+                    println!("Fatal error: No http binding configuration available.");
+                    ::std::process::exit(1);
+                }
+            }
+    }
+
+    // 8. Now let's crank up the http server.
+    match HttpServer::new(|| App::new().service(web::resource("/{name}/{id}/index.html").to(index))
+    ).bind(&bind_string) {
+        Ok(_result) => {
+            println!("Bind success.");
+            println!("Ctrl-C to stop.");
+            //_result.run();
+        }
+        Err(why) => {
+            println!("Bind failure: {}", why.description());
+            ::std::process::exit(1);
+        }
+    };
+
+}
---
+++ tests/config.rs
@@ -0,0 +1,320 @@
+use assert_cmd::prelude::*;
+use bookwerx_core_rust::constants as C;
+use predicates::prelude::*;
+use std::process::Command;
+
+/*
+These tests should be run one at a time so be sure to set RUST_TEST_THREADS=1 when executing the tests.
+
+These tests require access to a suitably configured mysql db.
+
+We cannot reasonably control the order of execution of these tests and each test can be affected by the state of the db, as left by the prior test.  Although it's tempting to require tests to cleanup the db after their use, said process can fail and result in subsequent hard-to-diagnose issues with other tests.  It's better to just brainwipe the db before each test.
+*/
+
+// Prepare the battlefield for the test.
+fn establish_initial_conditions() {
+
+    match mysql::Conn::new(C::TEST_CONN_STR) {
+        Ok(mut _conn) => {
+
+            match _conn.query(format!("DROP DATABASE IF EXISTS `{0}`;", C::TEST_DB_NAME)) {
+                Ok(_) => {
+                    //println!("drop and create success");
+                }
+                Err(_err) => {
+                    println!("{}", _err);
+                    ::std::process::exit(1);
+                }
+            };
+        }
+        Err(_err) => {
+            println!("{}", _err);
+            ::std::process::exit(1);
+        }
+    };
+}
+
+
+#[test] // 1.1
+fn conn_no_cli_no_env() -> Result<(), Box<std::error::Error>> {
+
+    establish_initial_conditions();
+
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+    cmd.assert()
+        .stdout(predicate::str::contains("Fatal error: No db connection string available."))
+        .failure();
+
+    Ok(())
+}
+
+#[test] // 1.2
+fn conn_no_cli_with_env() -> Result<(), Box<std::error::Error>> {
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+
+    cmd.env(C::CONN_KEY_ENV,C::TEST_CONN_STR);
+
+    cmd.assert()
+        .stdout(predicate::str::contains(format!("Accessing the db via connection string [{}], as set from the environment.", C::TEST_CONN_STR)))
+        .stdout(predicate::str::contains(format!("Fatal error: No database specified.")))
+        .failure();
+
+    Ok(())
+}
+
+#[test] // 1.2
+fn conn_with_cli_no_env() -> Result<(), Box<std::error::Error>> {
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+
+    cmd.arg(format!("--{}", C::CONN_KEY_CLI))
+        .arg(C::TEST_CONN_STR);
+
+    cmd.assert()
+        .stdout(predicate::str::contains(format!("Accessing the db via connection string [{}], as set from the command line.", C::TEST_CONN_STR)))
+        .stdout(predicate::str::contains(format!("Fatal error: No database specified.")))
+        .failure();
+
+    Ok(())
+}
+
+#[test] // 1.3
+// The value set in the command line should override whatever is in the environment.
+fn conn_with_cli_with_env() -> Result<(), Box<std::error::Error>> {
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+
+    cmd.env(C::CONN_KEY_ENV,C::TEST_CONN_STR);
+
+    cmd.arg(format!("--{}", C::CONN_KEY_CLI))
+        .arg(C::TEST_CONN_STR);
+
+    cmd.assert()
+        .stdout(predicate::str::contains(format!("Accessing the db via connection string [{}], as set from the command line.", C::TEST_CONN_STR)))
+        .stdout(predicate::str::contains(format!("Fatal error: No database specified.")))
+        .failure();
+
+    Ok(())
+}
+
+#[test] // 2.1
+fn db_no_cli_no_env() -> Result<(), Box<std::error::Error>> {
+
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+
+    // This is necessary to make the test proceed far enough to test what we want.
+    cmd.env(C::CONN_KEY_ENV,C::TEST_CONN_STR);
+
+    cmd.assert()
+        .stdout(predicate::str::contains("Fatal error: No database specified."))
+        .failure();
+
+    Ok(())
+}
+
+#[test] // 2.2
+fn db_no_cli_with_env() -> Result<(), Box<std::error::Error>> {
+
+    establish_initial_conditions();
+
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+
+    // This is necessary to make the test proceed far enough to test what we want.
+    cmd.env(C::CONN_KEY_ENV,C::TEST_CONN_STR)
+
+        // This is what we're really testing
+        .env(C::DB_KEY_ENV,C::TEST_DB_NAME);
+
+    cmd.assert()
+        .stdout(predicate::str::contains(format!("Using database [{}], as set from the environment.", C::TEST_DB_NAME)))
+        .stdout(predicate::str::contains(format!("Connected to [{}]", C::TEST_CONN_STR)))
+        .stdout(predicate::str::contains(format!("Fatal error: Unknown database [{}].", C::TEST_DB_NAME)))
+        .failure();
+
+    Ok(())
+}
+
+#[test] // 2.2
+fn db_with_cli_no_env() -> Result<(), Box<std::error::Error>> {
+
+    establish_initial_conditions();
+
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+
+    // This is necessary to make the test proceed far enough to test what we want.
+    cmd.env(C::CONN_KEY_ENV,C::TEST_CONN_STR)
+
+        // This is what we're really testing
+        .arg(format!("--{}", C::DB_KEY_CLI))
+        .arg(C::TEST_DB_NAME);
+
+    cmd.assert()
+        .stdout(predicate::str::contains(format!("Fatal error: Unknown database [{}].", C::TEST_DB_NAME)))
+        .failure();
+
+    Ok(())
+}
+
+#[test] // 2.3
+// The value set in the command line should override whatever is in the environment.
+fn db_with_cli_with_env() -> Result<(), Box<std::error::Error>> {
+
+    establish_initial_conditions();
+
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+
+    // This is necessary to make the test proceed far enough to test what we want.
+    cmd.env(C::CONN_KEY_ENV,C::TEST_CONN_STR)
+
+        // This is what we're really testing
+        .env(C::DB_KEY_ENV,"example-db-name-from-env")
+
+        .arg(format!("--{}", C::DB_KEY_CLI))
+        .arg(C::TEST_DB_NAME);
+
+    cmd.assert()
+        .stdout(predicate::str::contains(format!("Fatal error: Unknown database [{}].", C::TEST_DB_NAME)))
+        .failure();
+
+    Ok(())
+}
+
+#[test] // 3.1
+fn init_no_cli_no_env() -> Result<(), Box<std::error::Error>> {
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+
+    // This is necessary to make the test proceed far enough to test what we want.
+    cmd.env(C::CONN_KEY_ENV,C::TEST_CONN_STR)
+    .env(C::DB_KEY_ENV,C::TEST_DB_NAME);
+
+    // This is what we're really testing
+    // Testing nada!
+
+    cmd.assert()
+        .failure()
+        .stdout(predicate::str::contains("initializing").count(0)); // we don't see this
+    Ok(())
+}
+
+
+
+// 3.2
+
+#[test] // 3.2.1
+fn init_with_nonexistent_seed_file_via_cli() -> Result<(), Box<std::error::Error>> {
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+
+    // This is necessary to make the test proceed far enough to test what we want.
+    cmd.env(C::CONN_KEY_ENV,C::TEST_CONN_STR)
+        .env(C::DB_KEY_ENV,C::TEST_DB_NAME)
+
+        // This is what we're really testing
+        .arg(format!("--{}", C::INIT_KEY_CLI))
+        .arg("no such seed file");
+
+    cmd.assert()
+        .stdout(predicate::str::contains(format!("Initializing the db with seed file [{}], as set from the command line.", "no such seed file")))
+        .stdout(predicate::str::contains(format!("Couldn't open [{}]: entity not found", "no such seed file")))
+        .failure();
+
+    Ok(())
+}
+
+#[test] // 3.2.2
+fn init_with_invalid_seed_file_via_env() -> Result<(), Box<std::error::Error>> {
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+
+    // This is necessary to make the test proceed far enough to test what we want.
+    cmd.env(C::CONN_KEY_ENV,C::TEST_CONN_STR)
+        .env(C::DB_KEY_ENV,C::TEST_DB_NAME)
+
+        // This is what we're really testing
+        .env(C::INIT_KEY_ENV,C::INVALID_SEED_FILE);
+
+    cmd.assert()
+        .stdout(predicate::str::contains(format!("Initializing the db with seed file [{}], as set from the environment.", C::INVALID_SEED_FILE)))
+        .stdout(predicate::str::contains(format!("The seed file does not contain valid SQL.")))
+        .failure();
+
+    Ok(())
+}
+
+#[test] // 3.2.3
+fn init_with_valid_seed_file_cli_override_env() -> Result<(), Box<std::error::Error>> {
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+
+    // This is necessary to make the test proceed far enough to test what we want.
+    cmd.env(C::CONN_KEY_ENV,C::TEST_CONN_STR)
+        .env(C::DB_KEY_ENV,C::TEST_DB_NAME)
+
+        // This is what we're really testing...
+        .env(C::INIT_KEY_ENV,C::INVALID_SEED_FILE)
+
+        // ... the CLI should override the env
+        .arg(format!("--{}", C::INIT_KEY_CLI))
+        .arg(C::MYSQL_SEED_FILE);
+
+    cmd.assert()
+        .stdout(predicate::str::contains(format!("The seed has germinated.")))
+        .failure();
+
+    Ok(())
+}
+
+#[test] // 4.1
+fn bind_no_cli_no_env() -> Result<(), Box<std::error::Error>> {
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+
+    // This is necessary to make the test proceed far enough to test what we want.
+    cmd.env(C::CONN_KEY_ENV,C::TEST_CONN_STR)
+        .env(C::DB_KEY_ENV,C::TEST_DB_NAME)
+        .env(C::INIT_KEY_ENV,C::MYSQL_SEED_FILE);
+
+    // This is what we're really testing
+    // Testing nada!
+
+    cmd.assert()
+        .stdout(predicate::str::contains("Fatal error: No http binding configuration available."))
+        .failure();
+    Ok(())
+}
+
+#[test] // 4.2
+fn bind_no_cli_with_env() -> Result<(), Box<std::error::Error>> {
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+
+    // This is necessary to make the test proceed far enough to test what we want.
+    cmd.env(C::CONN_KEY_ENV,C::TEST_CONN_STR)
+        .env(C::DB_KEY_ENV,C::TEST_DB_NAME)
+        .env(C::INIT_KEY_ENV,C::MYSQL_SEED_FILE)
+
+    // This is what we're really testing
+        .env(C::BIND_KEY_ENV,C::TEST_BIND);
+
+    cmd.assert()
+        .stdout(predicate::str::contains(format!("The HTTP server will bind to [{}], as specified in the environment.", C::TEST_BIND)))
+        .stdout(predicate::str::contains(format!("Bind failure")))
+        .failure();
+
+    Ok(())
+}
+
+#[test] // 4.2
+fn bind_with_cli_no_env() -> Result<(), Box<std::error::Error>> {
+
+    let mut cmd = Command::cargo_bin(C::CARGO_BIN)?;
+
+    // This is necessary to make the test proceed far enough to test what we want.
+    cmd.env(C::CONN_KEY_ENV,C::TEST_CONN_STR)
+        .env(C::DB_KEY_ENV,C::TEST_DB_NAME)
+        .env(C::INIT_KEY_ENV,C::MYSQL_SEED_FILE)
+
+        // This is what we're really testing
+        .arg(format!("--{}", C::BIND_KEY_CLI))
+        .arg(C::TEST_BIND);
+
+    cmd.assert()
+        .stdout(predicate::str::contains(format!("The HTTP server will bind to [{}], as specified from the command line.", C::TEST_BIND)))
+        .stdout(predicate::str::contains(format!("Bind failure")))
+        .failure();
+
+    Ok(())
+}
